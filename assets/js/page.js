cc69c9e0238a642556ab6c4eb0203b438dc4c40b
Terra Vidhal <infiniterraetincelle@gmail.com>
2022-12-01 13:49:10 +0100

    final_css_uniquement

---
+++ assets/js/page.js
@@ -0,0 +1,44 @@
+
+
+window.onload = () =>{
+
+    /******** DECLARATION DES VARIABLES ************/
+    
+    var asideLeft = document.getElementById("aside-left");
+    var IconBurger = document.querySelector(".header-icon-burger i");
+    var menuLists = document.querySelectorAll(".menu-lists > li > a");
+    
+    /********  AFFICHACHE/MASQUAGE  PARTIE ASIDE-LEFT ************/
+        
+    IconBurger.addEventListener("click", ()=>{
+       
+        if (getComputedStyle(asideLeft).zIndex == "0" ) {
+            asideLeft.style.zIndex = "555";
+        }
+        else{
+            asideLeft.style.zIndex = "0";
+        }
+    })
+
+    /******** MASQUAGE  PARTIE ASIDE-LEFT ************/
+
+    for (let index = 0; index < menuLists.length; index++) {
+        const menuList = menuLists[index];
+        menuList.addEventListener("click", ()=>{
+            
+            asideLeft.style.zIndex = "0";
+        })
+        
+    } 
+
+}
+
+
+
+
+
+
+
+
+
+
